7295e11cc7204a9c70509e3b492720ebf37a96a0
Kamil Borowski <kamilborowski91@gmail.com>
2020-05-02 18:09:39 +0100

    added functions before_request, after_request

--- app.py
+++ app.py
@@ -5,10 +5,24 @@ import models
 app = Flask(__name__)
 
 
+@app.before_request
+def before_request():
+    """Connect to the database before each request."""
+    g.db = models.DATABASE
+    g.db.connect()
+
+
+@app.after_request
+def after_request(response):
+    """Close the database connection after each request."""
+    g.db.close()
+    return response
+
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
